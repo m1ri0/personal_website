15b357644b0025bd001c5afffe910dcb4feddf03
Mario <marioluciocollinettijr@hotmail.com>
2026-08-31 18:01:20 -0300

    starting to getting better

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -2,14 +2,27 @@ import './about.css';
 
 export default function About() {
     return (
-        <section className="about-section">
-            <div className="about-container">
-                <h2>About Me</h2>
-                <p>
-                    I am a passionate software developer with a strong interest in web development and programming. 
-                    I enjoy creating efficient and user-friendly applications, and I am always eager to learn new technologies and improve my skills.
-                </p>
+        <div className="about-container">
+
+            <div className="about-terminal">
+                <div className="about-header">
+                    <span className="dot dot-red"></span>
+                    <span className="dot dot-muted"></span>
+                    <span className="dot dot-muted"></span>
+                </div>
+                <div className="about-body">
+                    <h2>About Me</h2>
+                    <p>
+                        I am a passionate software developer with a strong interest in web development and programming.
+                        I enjoy creating efficient and user-friendly applications, and I am always eager to learn new technologies and improve my skills.
+                    </p>
+                </div>
             </div>
-        </section>
+
+            <div className="about-image">
+                <img src="../../assets/profile.jpeg" alt="Profile" />
+            </div>
+
+        </div>
     )
 }
